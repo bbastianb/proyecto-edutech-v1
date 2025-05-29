a398af7246f504d14b2c9b3838709da4b17281ef
Aurora <aur.gonzalez@duocuc.cl>
2025-05-29 09:53:32 -0400

    Rama instructor completa real real

--- src/main/java/com/proyecto/Edutech_v1/proyecto/Edutech_v1/repository/InstructorRepository.java
+++ src/main/java/com/proyecto/Edutech_v1/proyecto/Edutech_v1/repository/InstructorRepository.java
@@ -1,5 +1,10 @@
 package com.proyecto.Edutech_v1.proyecto.Edutech_v1.repository;
 
-public class InstructorRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.proyecto.Edutech_v1.proyecto.Edutech_v1.model.Instructor;
+
+    @Repository
+public interface InstructorRepository extends JpaRepository<Instructor, Long> {
 }
