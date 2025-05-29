c2023edbb77d2276a0f3bb866db3b193e48435e8
Aurora <aur.gonzalez@duocuc.cl>
2025-05-29 09:37:28 -0400

    Rama Gerente Cursos completa

---
+++ src/main/java/com/proyecto/Edutech_v1/proyecto/Edutech_v1/controller/GerenteCursosController.java
@@ -0,0 +1,5 @@
+package com.proyecto.Edutech_v1.proyecto.Edutech_v1.controller;
+
+public class GerenteCursosController {
+
+}
---
+++ src/main/java/com/proyecto/Edutech_v1/proyecto/Edutech_v1/repository/GerenteCursosRepository.java
@@ -0,0 +1,5 @@
+package com.proyecto.Edutech_v1.proyecto.Edutech_v1.repository;
+
+public class GerenteCursosRepository {
+
+}
